5f89fbb57a1b92095abe207a535f65f1bc8011c3
Stanislav Pivovartsev <stanislav_pivovartsev@mail.ru>
2018-04-09 01:37:22 +0300

    Remove obsolete header Access-Control-Allow-Origin from getConfig endpoint

--- src/AppBundle/Controller/LockController.php
+++ src/AppBundle/Controller/LockController.php
@@ -220,7 +220,6 @@ class LockController extends FOSRestController implements ClassResourceInterface
         file_put_contents($newfile,$data);
 //        header('Content-Description: File Transfer');
         header ("Content-Type: application/json");
-        header("Access-Control-Allow-Origin: *");
 //        header ("Accept-Ranges: bytes");
 //        header ("Content-Length: ".filesize($newfile));
 //        header('Content-Disposition: attachment; filename="'.basename($newfile).'"');
