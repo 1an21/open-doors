40e35f1afa395975c5e7d990ad25dc28f0b6f383
an21 <you@example.com>
2018-04-11 14:01:09 +0300

    add condition in entranceController, if no key or lock return 404, otherwise 200; delete unnecessary methods

--- src/AppBundle/Controller/EntranceController.php
+++ src/AppBundle/Controller/EntranceController.php
@@ -42,15 +42,15 @@ class EntranceController extends FOSRestController implements ClassResourceInter
      *     }
      * )
      */
-    public function getAction($id)
-    {
-        $key = $this->getEntranceRepository()->findEntranceQuery($id)->getOneOrNullResult();
-        if ($key === null) {
-            return new View("Dont exist key with id $id for lock $lock");
-        }
-        
-        return $key;
-    }
+//    public function getAction($id)
+//    {
+//        $key = $this->getEntranceRepository()->findEntranceQuery($id)->getOneOrNullResult();
+//        if ($key === null) {
+//            return new View("Dont exist key with id $id for lock $lock");
+//        }
+//
+//        return $key;
+//    }
 
     /**
      * Gets a collection of keys for locks )
@@ -74,17 +74,11 @@ class EntranceController extends FOSRestController implements ClassResourceInter
                 ->where('l.lock_name LIKE :tag OR k.tag LIKE :tag OR en.result LIKE :tag' )
                 ->setParameter('tag', '%' . $request->query->getAlnum('filter') . '%');
         }
-        $paginator  = $this->get('knp_paginator');
-        $pagination = $paginator->paginate(
-        $queryBuilder, /* query NOT result */
-        $request->query->getInt('page', 1)/*page number*/,
-        10/*limit per page*/
-    );
-        $queryBuilder->getQuery()->getResult();
+
         return $this->get('knp_paginator')->paginate(
             $queryBuilder->getQuery(), /* query NOT result */
             $request->query->getInt('page', 1), /*page number*/
-            $request->query->getInt('limit', 5)/*limit per page*/
+            $request->query->getInt('limit', 10)/*limit per page*/
         );
         // return $this->getEntranceRepository()->findLockQuery()->getResult();
     }
@@ -92,14 +86,14 @@ class EntranceController extends FOSRestController implements ClassResourceInter
 
 
     /**
-     * Add a new lock key relationship
+     * Add logs of entrances from LogEntrancesService
      * @param Request $request
      * @return View|\Symfony\Component\Form\Form
      *
      * @ApiDoc(
      *     output="AppBundle\Entity\Entrance",
      *     statusCodes={
-     *         201 = "Returned when a new lock key relationship has been successful created",
+     *         200 = "Returned when a lock and key are in database",
      *         404 = "Return when not found"
      *     }
      * )
@@ -113,178 +107,19 @@ class EntranceController extends FOSRestController implements ClassResourceInter
         $fieldtime=$request->request->get('time');
 
         $lock=$em->getRepository('AppBundle:Lock')->findOneBy(array('lock_name' => $fieldlockname));
-        $id_lock=$lock->getId();
-
         $key=$em->getRepository('AppBundle:Key')->findOneBy(array('tag' => $fieldtag));
-        $id_key=$key->getId();
-        
-        $em->flush();
-        $entrance=new Entrance();
-        $entrance->setLock($lock);
-        $entrance->setKey($key);
-        $entrance->setResult($fieldresult);
-        $entrance->setTime($fieldtime);
-        $em->persist($entrance);
-        $em->flush();
-        
-        return new View("OK", Response::HTTP_OK);
-    }
+        if($key!=null & $lock!=null) {
+            $entrance = new Entrance();
+            $entrance->setLock($lock);
+            $entrance->setKey($key);
+            $entrance->setResult($fieldresult);
+            $entrance->setTime($fieldtime);
+            $em->persist($entrance);
+            $em->flush();
 
-    /**
-     * Totally update 
-     * @param Request $request
-     * @param int     $id
-     * @param int     $lock
-     * @return View|\Symfony\Component\Form\Form
-     *
-     * @ApiDoc(
-     *     input="AppBundle\Form\Type\EntranceType",
-     *     output="AppBundle\Entity\Entrance",
-     *     statusCodes={
-     *         204 = "Returned when an existing lock key relationship has been successful updated",
-     *         400 = "Return when errors",
-     *         404 = "Return when not found"
-     *     }
-     * )
-     */
-    public function putAction(Request $request, $id)
-    {
-
-        $key = $this->getEntranceRepository()->findOneBy(array('key'=>$id, 'lock'=>$lock));
-
-        if ($key === null) {
-            return new View("Doesnt exist", Response::HTTP_NOT_FOUND);
+            return new View("OK", Response::HTTP_OK);
         }
-
-        $form = $this->createForm(EntranceType::class, $key, [
-            'csrf_protection' => false,]);
-
-        $form->submit($request->request->all());
-
-        if (!$form->isValid()) {
-            return $form;
-        }
-
-        $em = $this->getDoctrine()->getManager();
-        $em->flush();
-
-        $routeOptions = [
-            'id' => $key->getId(),
-            '_format' => $request->get('_format'),
-        ];
-
-        $this->routeRedirectView('', $routeOptions, Response::HTTP_OK);
-        $id=$key->getId();
-        return $this->getEntranceRepository()->findIdQuery($id)->getOneOrNullResult();
-    }
-
-
-    /**
-     * Update
-     * @param Request $request
-     * @param int     $id
-     * @param int     $lock
-     * @return View|\Symfony\Component\Form\Form
-     *
-     * @ApiDoc(
-     *     input="AppBundle\Form\Type\EntranceType",
-     *     output="AppBundle\Entity\Entrance",
-     *     statusCodes={
-     *         204 = "Returned when an existing lock key relationship has been successful updated",
-     *         400 = "Return when errors",
-     *         404 = "Return when not found"
-     *     }
-     * )
-     */
-    public function patchAction(Request $request, $id)
-    {
-        /**
-         * @var $key key
-         */
-        $key = $this->getEntranceRepository()->findOneBy(array('key'=>$id, 'lock'=>$lock));
-        if ($key === null) {
-            return new View("Doesnt exist", Response::HTTP_NOT_FOUND);
-        }
-        $form = $this->createForm(EntranceType::class, $key, [
-            'csrf_protection' => false,
-        ]);
-        $form->submit($request->request->all(), false);
-        if (!$form->isValid()) {
-            return $form;
-        }
-        $em = $this->getDoctrine()->getManager();
-        $em->flush();
-        $routeOptions = [
-            'id' => $key->getId(),
-            '_format' => $request->get('_format'),
-        ];
-        $this->routeRedirectView('', $routeOptions, Response::HTTP_OK);
-        $id=$key->getId();
-        return $this->getEntranceRepository()->findIdQuery($id)->getOneOrNullResult();
-    }
-
-
-    /**
-     * Delete a lock key relationship
-     * @param int $id
-     * @param int $lock
-     * @return View
-     *
-     * @ApiDoc(
-     *     statusCodes={
-     *         204 = "Returned when an existing lock key relationship has been successful deleted",
-     *         404 = "Return when not found"
-     *     }
-     * )
-     */
-    public function deleteAction( $id)
-    {
-        $key = $this->getEntranceRepository()->findEntranceQuery($lock, $id)->getOneOrNullResult();
-         if ($key=== null) {
-            return new View("This id $id doesnt exist", Response::HTTP_NOT_FOUND); }
-        // return new View("Deleted key $id for lock $lock");
-        
-        $em = $this->get('doctrine')->getManager();
-        $locks=$em->getRepository('AppBundle:Lock')->findOneById($lock);
-        $kkey=$em->getRepository('AppBundle:Key')->findOneById($id);
-        $em->flush();
-
-        $Entrance=new Entrance();
-        $Entrance->setLock($locks);
-        $Entrance->setKey($kkey);
-
-        $redis_ip=$this->container->getParameter('redis_ip');
-        $redis_ports=$this->container->getParameter('redis_ports');
-        $redis_schemes=$this->container->getParameter('redis_schemes');
-
-        try {
-            $predisClient = new Client(array(
-            'scheme'   => $redis_schemes,
-            'host'     => $redis_ip,
-            'port'     => $redis_ports
-        ));
-        }
-        catch (Exception $e){
-            die($e->getMessage());
-        }    
-
-        $id=$Entrance->getLock();
-        $ids=$Entrance->getKey();
-
-
-        $lockss=$em->getRepository('AppBundle:Lock')->findOneById($id);
-        $name_lock=$lockss->getLockName();
-
-        $keys=$em->getRepository('AppBundle:Key')->findOneById($ids);
-        $tag_key=$keys->getTag();
-
-
-        $lock_key= $name_lock.':'.$tag_key;
-        $predisClient->del($lock_key);
-        
-        $em->remove($key);
-        $em->flush();
-        return new View("Deleted key ");
+        else return new View("Not found this lock or key", Response::HTTP_NOT_FOUND);
     }
 
     /**
@@ -293,14 +128,6 @@ class EntranceController extends FOSRestController implements ClassResourceInter
     private function getEntranceRepository()
     {
         return $this->get('crv.doctrine_entity_repository.entrance');
-    }
-    private function getLockRepository()
-    {
-        return $this->get('crv.doctrine_entity_repository.lock');
-    }
-    private function getLockKeyRepository()
-    {
-        return $this->get('crv.doctrine_entity_repository.lockkey');
     }
 
 }
